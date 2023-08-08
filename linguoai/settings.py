b5acff86637be3d81eeb11f58cca550f7ff8a14e
Nick Rogerson <nickrogerson@192.168.0.101>
2023-08-08 20:31:09 +0800

    Update

--- linguoai/settings.py
+++ linguoai/settings.py
@@ -148,7 +148,7 @@ USE_I18N = True
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = 'static/'
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
--- linguoai/urls.py
+++ linguoai/urls.py
@@ -16,11 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from wagtail.admin import urls as wagtailadmin_urls
-from wagtail import urls as wagtail_urls
-from wagtail.documents import urls as wagtaildocs_urls
-from django.conf import settings
-from django.conf.urls.static import static
+# from wagtail.admin import urls as wagtailadmin_urls
+# from wagtail import urls as wagtail_urls
+# from wagtail.documents import urls as wagtaildocs_urls
+# from django.conf import settings
+# from django.conf.urls.static import static
 
 urlpatterns = [
     # path('cms/', include(wagtailadmin_urls)),
