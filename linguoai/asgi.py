61e62d0fd4c2ebf75edc3b39ff96ce9c92a6901a
Nick Rogerson <nickrogerson@myhost.local>
2023-09-02 15:59:01 +0800

    Channels Update

--- linguoai/asgi.py
+++ linguoai/asgi.py
@@ -1,18 +1,25 @@
-"""
-ASGI config for LinguoAI project.
+import os
 
-It exposes the ASGI callable as a module-level variable named ``application``.
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.security.websocket import AllowedHostsOriginValidator
+from django.core.asgi import get_asgi_application
 
-For more information on this file, see
-https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
-"""
+from members.routing import websocket_urlpatterns
 
-# import os
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
+# Initialize Django ASGI application early to ensure the AppRegistry
+# is populated before importing code that may import ORM models.
+django_asgi_app = get_asgi_application()
 
-# from django.core.asgi import get_asgi_application
+# import members.routing
 
-# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'linguoai.settings')
-
-# application = get_asgi_application()
-
+application = ProtocolTypeRouter(
+    {
+        "http": django_asgi_app,
+        "websocket": AllowedHostsOriginValidator(
+            AuthMiddlewareStack(URLRouter(websocket_urlpatterns))
+        ),
+    }
+)
 
