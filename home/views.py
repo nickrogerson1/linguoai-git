f869e500c58efb9cd2c20e3f0d2d5f641fe02e78
Nick Rogerson <nickrogerson@myhost.local>
2023-09-12 19:15:01 +0800

    Update

--- home/views.py
+++ home/views.py
@@ -1,8 +1,14 @@
+from django.shortcuts import render
 import datetime
 from django.views.generic import FormView
 from .forms import ContactForm
 from django.http import JsonResponse
 from django.core.mail import send_mail
+
+# Handles issue of importing from another folder/app
+import sys
+sys.path.append('.')
+from members.pricing import *
 
 
 
@@ -13,7 +19,25 @@ class Homepage(FormView):
    
     # Add the year to the context (for the get)
     def get_context_data(self, **kwargs):
-        kwargs = {'year' : datetime.date.today().year}
+        # Dynamically send over the pricing
+        ielts_writing_task_2 = PRICING['ielts_writing_task_2']
+        ielts_usd = ielts_writing_task_2['USD']
+        ielts_cny = ielts_writing_task_2['CNY']
+        corrected = PRICING['corrected_results']
+        corrected_usd = corrected['USD']
+        corrected_cny = corrected['CNY']
+        improved = PRICING['improved_results']
+        improved_usd = improved['USD']
+        improved_cny = improved['CNY']
+
+        kwargs = {'year' : datetime.date.today().year,
+                  'ielts_usd' : ielts_usd,
+                  'ielts_cny' : ielts_cny,
+                  'corrected_usd' : corrected_usd,
+                  'corrected_cny' : corrected_cny,
+                  'improved_usd' : improved_usd,
+                  'improved_cny' : improved_cny  
+            }
         if "form" not in kwargs:
             kwargs["form"] = self.get_form()
         print(kwargs)
@@ -47,5 +71,5 @@ class Homepage(FormView):
             # Tell the modal it can close
             return JsonResponse({'formSubmitSuccess' : True}, status = 200)
         else:
-            # Currently unable to inform the modal it's invalid
+            # Currently unable to inform the modal it's invalid so done with JS
             return self.form_invalid(form)
